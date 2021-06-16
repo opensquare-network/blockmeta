f77e04849953aefc26bcd61374206e287f6b2acf
Yongfeng LI <wliyongfeng@gmail.com>
2021-06-16 21:21:30 +0800

    Add pm2 task

---
+++ ecosystem.config.js
@@ -0,0 +1,47 @@
+const commonPart = {
+  script: "src/index.js",
+  log_date_format: "YYYY-MM-DD HH:mm Z",
+  env: {
+    NODE_ENV: "development",
+  },
+  env_production: {
+    NODE_ENV: "production",
+  },
+};
+
+function getEnvConfig(chainName) {
+  return {
+    env: {
+      ...commonPart.env,
+      CHAIN: chainName,
+    },
+    env_production: {
+      ...commonPart.env_production,
+      CHAIN: chainName,
+    },
+  };
+}
+
+const dotScanConfig = {
+  ...commonPart,
+  ...getEnvConfig("polkadot"),
+};
+
+const ksmScanConfig = {
+  ...commonPart,
+  ...getEnvConfig("kusama"),
+};
+
+module.exports = {
+  apps: [
+    // prod-scan
+    {
+      name: "meta-scan-dot",
+      ...dotScanConfig,
+    },
+    {
+      name: "meta-scan-ksm",
+      ...ksmScanConfig,
+    },
+  ]
+}
