2609dbfee467c0ae3a80e5e5a1b60c4b868fb8f7
Yongfeng LI <wliyongfeng@gmail.com>
2021-11-08 16:39:13 +0800

    Save provider

--- src/chain/provider.js
+++ src/chain/provider.js
@@ -1,12 +1,16 @@
 const { WsProvider } = require("@polkadot/api");
 
-async function getOnlyProvider() {
-  const endPoint = process.env.WS_ENDPOINT;
-  if (!endPoint) {
-    throw new Error("WS_ENDPOINT not set");
-  }
+let provider = null;
 
-  const provider = new WsProvider(endPoint, 1000);
+async function getOnlyProvider() {
+  if (!provider) {
+    const endPoint = process.env.WS_ENDPOINT;
+    if (!endPoint) {
+      throw new Error("WS_ENDPOINT not set");
+    }
+
+    provider = new WsProvider(endPoint, 1000);
+  }
 
   await provider.isReady
   return provider
