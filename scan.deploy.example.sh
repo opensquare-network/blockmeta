b8ff8c97324ca597a4669e1165beb6faf9973dd4
Yongfeng LI <wliyongfeng@gmail.com>
2024-05-05 10:13:22 +0800

    Add refresh deploy example script

--- scan.deploy.example.sh
+++ scan.deploy.example.sh
@@ -1,1 +1,1 @@
-NODE_ENV=production pm2 start src/index.js --name meta-scan --log-date-format 'YYYY-MM-DD HH:mm Z' --env production
+NODE_ENV=production pm2 start src/index.js --name prod-meta-polkadot --log-date-format 'YYYY-MM-DD HH:mm Z' --env production --max-memory-restart 600M
